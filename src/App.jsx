fbb8677cddc84872ecdb35bf16beb45ebf284c21
Mohd Aftab Idrisi <aftab.idrisi0731@gmail.com>
2024-07-25 13:03:06 +0530

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import NavBar from "./components/header";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+const App = () => {
   return (
-    <>
-      <div>
-        <p>Hello World</p>
-      </div>
-    </>
+    <div>
+      <NavBar />
+    </div>
   );
-}
+};
 
 export default App;
