11bb13769569403d7fad6acc7b2d8b2d8eaf4a9f
Mohd Aftab Idrisi <aftab.idrisi0731@gmail.com>
2024-07-26 11:48:18 +0530

    footer

--- src/components/blog.jsx
+++ src/components/blog.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import NavBar from "./navbar";
+import Footer from "./footer";
 
 function Blog() {
   return (
     <>
       <NavBar />
       <div>Blog Page</div>
+      <Footer />
     </>
   );
 }
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./home.css";
 import NavBar from "./navbar";
+import Footer from "./footer";
 import images from "../images/bookmark_banner.jpg";
 
 function Home() {
@@ -10,6 +11,7 @@ function Home() {
       <div className="home_banner-container">
         <img src={images} alt="" />
       </div>
+      <Footer />
     </>
   );
 }
