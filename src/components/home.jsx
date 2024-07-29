bd277b59832e29ebd7012f0d684f2c7b2474137f
Mohd Aftab Idrisi <aftab.idrisi0731@gmail.com>
2024-07-29 13:00:04 +0530

    Aashiv ka kaam Rohan kiya

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -17,28 +17,44 @@ function Home() {
       </div>
       <div className="home-section">
         <h1 className="right-text">Collect</h1>
-        <p>Save and tag your online resources for easy access anytime, anywhere</p>
+        <p className="right-text">
+          Save and tag your online resources for easy access anytime, anywhere
+        </p>
       </div>
       <div className="home-section gray">
         <h1 className="left-text">Annotate</h1>
-        <p>Annotate web pages and PDFs directly as you browse online </p>
+        <p className="left-text">
+          Annotate web pages and PDFs directly as you browse online
+        </p>
       </div>
       <div className="home-section">
         <h1 className="right-text">Organize</h1>
-        <p>Organize your links, references and personal input to create a structured research base through Outliner</p>
+        <p className="right-text">
+          Organize your links, references and personal input to create a
+          <br></br>
+          structured research base through Outliner
+        </p>
       </div>
       <div className="home-section gray">
         <h1 className="left-text">Summarize</h1>
-        <p>Generate concise summaries of lengthy articles and documents with just one click
+        <p className="left-text">
+          Generate concise summaries of lengthy articles and documents with just
+          one click
         </p>
       </div>
       <div className="home-section">
         <h1 className="right-text">Share</h1>
-        <p>Share your research with friends, classmates, colleagues or associates</p>
+        <p className="right-text">
+          Share your research with friends, classmates, colleagues or associates
+        </p>
       </div>
       <div className="home-section gray">
         <h1 className="left-text">Discuss</h1>
-        <p>Engage in conversations directly within the content, enhancing the learning and research experience</p>
+        <p className="left-text">
+          Engage in conversations directly within the content, enhancing the
+          <br></br>
+          learning and research experience
+        </p>
       </div>
       <Footer />
     </>
